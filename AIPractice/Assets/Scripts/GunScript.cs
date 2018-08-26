7d7156639190640ad27ae70e312b9ee799523b03
Aylin Görgün <34216243+Aylin1360@users.noreply.github.com>
2018-08-27 01:06:31 +0300

    Mon 00:52

---
+++ AIPractice/Assets/Scripts/GunScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public GameObject impactEffect;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }		
+	}
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            target target = hit.transform.GetComponent<target>();
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+
+            }
+
+            GameObject impactGo =Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo, 0.2f);
+        }
+    }
+}
