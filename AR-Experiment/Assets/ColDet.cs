58c255b45fa909e8a06616d54cfdec43a02be920
Aylin Görgün <aylingrgn2@gmail.com>
2019-03-14 02:27:47 +0300

    AR Experiment added.

---
+++ AR-Experiment/Assets/ColDet.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColDet : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit");
+        }
+    }
+}
---
+++ AR-Experiment/Assets/vbButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class vbButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject vbBtnObj;
+  
+    // Use this for initialization
+    void Start()
+    {
+        vbBtnObj = GameObject.Find("VirtualButton");
+        vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+       
+        Debug.Log("Button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+       
+        Debug.Log("Button released");
+    }
+}
+
